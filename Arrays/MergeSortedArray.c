e7800e0c2df821af8a2f8e406073c3deb1e3a28b
sarikanallabothula7-png <sarikanallabothula7@gmail.com>
2026-08-18 20:37:16 +0530

    Add Merge Sorted Array solution
    This program merges two sorted arrays into one sorted array in-place.

---
+++ Arrays/MergeSortedArray.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    printf("Enter number of elements in nums1: ");
+    scanf("%d", &m);
+
+    int nums1[m + 20];
+
+    printf("Enter nums1 elements: ");
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &nums1[i]);
+    }
+
+    printf("Enter number of elements in nums2: ");
+    scanf("%d", &n);
+
+    int nums2[n];
+
+    printf("Enter nums2 elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums2[i]);
+    }
+
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
+
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+
+    printf("Merged Sorted Array: ");
+    for (int x = 0; x < m + n; x++) {
+        printf("%d ", nums1[x]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
